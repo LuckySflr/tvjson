437da381852a0940299b1385db473e6ccfea3cef
LuckySflr <2859519691@qq.com>
2024-02-20 16:16:24 +0800

    Add publish_new.sh.

---
+++ publish_new.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+echo "hello, world!"
+git checkout dev
+
+cd myscript
+python tvbox.json.aes.enc.py
+cd ..
+cat my.enc.json > my.json
+git commit -am "Update my.json."
+
+git checkout src
+git checkout dev my.json
+git commit -am "Update my.json."
+
+git checkout master
+git checkout dev my.json
+git commit -am "Update my.json."
+
+git checkout dev
+
+git push gitlab --all
