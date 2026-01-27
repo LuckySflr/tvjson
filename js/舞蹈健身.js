dd4336528e5fd6fa8c24bcb3ecace2c846e96cf3
laptop <2859519691@qq.com>
2026-01-27 21:06:04 +0800

    Update bilibili_cookies.

--- js/舞蹈健身.js
+++ js/舞蹈健身.js
@@ -6,8 +6,12 @@ let siteKey = '';
 let siteType = 0;
 let searchable= 0;//searchable=1 启用搜索
 const PC_UA = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/115.0.0.0 Safari/537.361";
+<<<<<<< HEAD
 // let cookie = "DedeUserID=690781341;DedeUserID__ckMd5=cabc96906269c5b6;SESSDATA=2245ba24%2C1684212125%2C466fd%2Ab2;bili_jct=de6fdb60c10f8a83910aa55d79407b4e;"; // 可更换成自己的cookie
 let cookie = "https://gitee.com/luckyflower01/tvjson/raw/src/js/bilibili_cookies.txt"; // 可更换成自己的cookie
+=======
+let cookie = "https://gitee.com/luckyflower01/tvjson/raw/src/cookies/my.bilibili.cookie.txt"
+>>>>>>> 4b55537 (Update bilibili cookies.)
 
 async function request(reqUrl) {
   const res = await req(reqUrl, {
